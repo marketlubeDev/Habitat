31bfd24c79691c415ccc76ea95b387ccf4b72fb2
marketLube <marketlubes@gmail.com>
2025-05-01 13:15:56 +0530

    SL

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import Habitat from "./Habitat.tsx";
+// import Habitat from "./Habitat.tsx";
+import Hab from "./Hab.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Habitat />
+    <Hab />
   </StrictMode>
 );
